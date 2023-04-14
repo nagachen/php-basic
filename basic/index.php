6fc298e449435c221a629b72a89ba5e41dc9d7d6
nagachen <birdann99@gmail.com>
2023-04-14 13:13:22 +0800

    PHP基礎，變數與Ｃ常數及運算子(有些資料不足，可以參考筆記或老師的程式

---
+++ basic/index.php
@@ -0,0 +1,7 @@
+<?php  #php開始符號
+$name = 123;
+$$name = 2001;
+echo $chen;
+#或直接調用 $$name
+echo "<br>";
+echo $$name;
---
+++ basic/opraters.php
@@ -0,0 +1,7 @@
+<?php
+#基礎練習
+$a = 10;
+$b = 50;
+echo "a = $a";
+echo '<br>';
+echo "b = $b";
