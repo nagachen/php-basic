58a7eb56471648f6b89e866e28c10a9d2b09f34b
nagachen <birdann99@gmail.com>
2023-04-14 14:01:46 +0800

    斷行範例

--- flow.php
+++ flow.php
@@ -19,25 +19,25 @@
 //     echo "再接再勵";
 // }
 //switch case範例
-echo "<hr>";
-$level = "A";
-switch ($level) {
-    case "A":
-        echo "表現優良，請繼續保持";
-        break;
-    case "B":
-        echo "值得肯定，還有進步空間";
-        break;
-    case "C":
-        echo "需要更多的練習";
-        break;
-    case "D":
-        echo "需要加強基本功";
-        break;
-    default:
-        echo "是否無心學業？";
-}
+// echo "<hr>";
+// $level = "A";
+// switch ($level) {
+//     case "A":
+//         echo "表現優良，請繼續保持";
+//         break;
+//     case "B":
+//         echo "值得肯定，還有進步空間";
+//         break;
+//     case "C":
+//         echo "需要更多的練習";
+//         break;
+//     case "D":
+//         echo "需要加強基本功";
+//         break;
+//     default:
+//         echo "是否無心學業？";
+// }
 
-// 斷行
-// print('<H2>1234566788129887431943'.
-//         '123331414141</H2>');
+//斷行
+print('<H2>1234566788129887431943'.
+        '123331414141</H2>');
