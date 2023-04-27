4758d9d8e1a959c7d4974be2bff563554fdaae63
nagachen <birdann99@gmail.com>
2023-04-27 10:18:09 +0800

    月歷三種方式的做法

--- time/calen.php
+++ time/calen.php
@@ -56,6 +56,8 @@ echo "<td>四</td>";
 echo "<td>五</td>";
 echo "<td>六</td>";
 echo "</tr>";
+
+
 for($i=0;$i<$weeks;$i++){          #形成月歷
     echo "<tr>";
     for($j=0;$j<7;$j++){                           #每週第幾天
@@ -73,3 +75,84 @@ for($i=0;$i<$weeks;$i++){          #形成月歷
     echo "</tr>";
 }
 echo "</table>";
+
+//先畫出固定的表頭內容
+
+$days=[];
+
+//使用迴圈來畫出當前月的周數
+for($i=0;$i<$weeks;$i++){
+    //使用迴圈來畫出當周的天數
+    for($j=0;$j<7;$j++){
+        //判斷當周是否為第一周或最後一周
+        if(($i==0 && $j<$firstDateWeek) || (($i==$weeks-1) && $j>$finalDateWeek)){
+            $days[]='&nbsp';
+        }else{
+            $days[]=$j+7*$i-$firstWeekSpace;
+        }
+    }
+}
+
+/* echo "<pre>";
+print_r($days);
+echo "<pre>"; */
+
+echo "<table>";
+echo "<tr>";
+echo "<td>日</td>";
+echo "<td>一</td>";
+echo "<td>二</td>";
+echo "<td>三</td>";
+echo "<td>四</td>";
+echo "<td>五</td>";
+echo "<td>六</td>";
+echo "</tr>";
+for($i=0;$i<count($days);$i++){
+    echo ($i%7==0)?"<tr>":'';
+    echo "<td>";
+    echo $days[$i];
+    echo "</td>";
+    echo ($i%7==6)?"</tr>":'';
+}
+echo "</table>";
+
+
+#使用flexbox
+?>
+<style>
+  .calendar > div{
+        border:1px solid #ccc;
+        width:calc(100% / 7);
+        box-sizing: border-box;
+        
+        margin-left:-1px;  /*調整邊框重疊 -1會重疊*/
+        margin-top:-1px;   /*調整邊框重疊 -1會重疊*/
+        
+
+    }
+.calendar{
+    display:flex;
+    flex-wrap: wrap;
+    width:50%;
+    margin-left:1px;  /*調整邊框重疊拉回來*/
+    margin-top:1px;   /*調整邊框重疊拉回來*/
+}
+
+</style>
+<div class='calendar'>
+<div>日</div>
+<div>一</div>
+<div>二</div>
+<div>三</div>
+<div>四</div>
+<div>五</div>
+<div>六</div>
+<?php
+for($i=0;$i<count($days);$i++){
+    echo "<div> {$days[$i]} </div>";  /* 大括號內只能字串，不能做數值相加*/
+}
+
+?>
+
+    
+</div>
